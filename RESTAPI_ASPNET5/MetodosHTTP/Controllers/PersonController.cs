a28e8b5c678c0c5552877761c5e394cdd71ca1af
Paulo Henrique Silva <paulohsilva2001@gmail.com>
2021-10-05 23:36:32 -0300

    Criada interface e seu service

---
+++ RESTAPI_ASPNET5/MetodosHTTP/Controllers/PersonController.cs
@@ -0,0 +1,69 @@
+﻿using MetodosHTTP.Model;
+using MetodosHTTP.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetodosHTTP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+
+            return Ok(person);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+        
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Update(person)); 
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RESTAPI_ASPNET5/MetodosHTTP/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,70 @@
+﻿using MetodosHTTP.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MetodosHTTP.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> peoples = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                peoples.Add(person);
+            }
+            return peoples;
+        }
+
+      
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                Name = "Paulo",
+                Age = 15,
+                City = "São Paulo",
+                Phone = "1199999999"
+
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                Name = "Name" + i,
+                Age = i,
+                City = "City Name" + i,
+                Phone = "Phone Number" + i
+                
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
