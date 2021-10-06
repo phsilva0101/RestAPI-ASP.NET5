5811f424781a68ccae0e6f5b73f144d268484cb0
Paulo Henrique Silva <paulohsilva2001@gmail.com>
2021-10-05 23:35:57 -0300

    Criado projeto pessoa e sua entidade

---
+++ RESTAPI_ASPNET5/MetodosHTTP/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MetodosHTTP.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name {  get; set; }
+        public int Age { get; set; }
+        public string City {  get; set; }
+        public string Phone {  get; set; }
+
+    }
+}
